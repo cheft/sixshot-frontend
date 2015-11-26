bf86990bcb037e8d74f3c89fad9b2134b2a5b8ff
chenhaifeng <m@cheft.cn>
2015-11-26 14:19:18 +0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-#sixshot-frontend
+# sixshot-frontend
+
+  npm install
+  npm start
+
+> http://localhost:8888/webpack-dev-server/
---
+++ main.js
@@ -0,0 +1,6 @@
+require('css/app.css');
+var riot = require('riot');
+
+window.onload = function() {
+  riot.mount('viewport', {name: 'cheft'});
+};
