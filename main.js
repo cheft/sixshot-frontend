d0dc8356d3613df10166a794378e8ec24e2dfb78
chenhaifeng <m@cheft.cn>
2015-11-27 16:17:40 +0800

    add router

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+var riot = window.riot = require('riot');
 require('css/app.css');
-var riot = require('riot');
 
 window.onload = function() {
-  riot.mount('viewport', {name: 'cheft'});
-};
+  riot.route('/', function(path) {
+    riot.mount('viewport', {path: 'test2'});
+  });
+
+  riot.route('/*', function(path) {
+    riot.mount('viewport', {path: path});
+  });
+
+  riot.route.base('#!')
+  riot.route.start(true);
+}
