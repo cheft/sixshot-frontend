ca98d596cb7393caf99ba2e7a1fd4a42e84b6d6a
chenhaifeng <m@cheft.cn>
2015-11-27 18:05:38 +0800

    Add AdminLTE UI

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 var riot = window.riot = require('riot');
 require('css/app.css');
 
+var HOMEPAGE = 'homepage';
+
+var activeMenu = function(path) {
+  if(path === HOMEPAGE) {
+    path = '';
+  }
+
+  $('.sidebar-menu li.active').removeClass('active');
+  $('.sidebar-menu a[href="/#!' + path + '"]').parent().addClass('active');
+};
+
 window.onload = function() {
   riot.route('/', function(path) {
-    riot.mount('viewport', {path: 'test2'});
+    riot.mount('navigator');
+    riot.mount('container', {path: HOMEPAGE});
+    activeMenu(HOMEPAGE);
   });
 
   riot.route('/*', function(path) {
-    riot.mount('viewport', {path: path});
+    riot.mount('navigator');
+    riot.mount('container', {path: path});
+    activeMenu(path);
   });
 
   riot.route.base('#!')
