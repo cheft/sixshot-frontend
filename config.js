47e8ae00e9e69c777a0e31fcd8733dd08aa1c535
陈海峰 <cn.cheft@gmail.com>
2015-12-19 15:34:26 +0800

    增加参数设置功能

--- config.js
+++ config.js
@@ -1,5 +1,52 @@
 module.exports = {
-  apiPrefix: 'http://172.16.1.62/'
+  sceneMap: [
+    {
+      key: 'chat',
+      value: '聊天'
+    },
+    {
+      key: 'poem',
+      value: '诗歌'
+    },
+    {
+      key: 'idiom',
+      value: '成语'
+    },
+    {
+      key: 'arithmetic',
+      value: '算数'
+    },
+    {
+      key: 'english',
+      value: '英语'
+    },
+    {
+      key: 'solitaire',
+      value: '接龙'
+    },
+    {
+      key: 'solitaireidiom',
+      value: '成语接龙'
+    },
+    {
+      key: 'solitaire-verse',
+      value: '诗歌接龙'
+    },
+    {
+      key: 'chat',
+      value: '聊天'
+    },
+    {
+      key: 'motion',
+      value: '运动'
+    },
+    {
+      key: 'shot-chat',
+      value: '短聊天'
+    },
+  ],
+
+  apiPrefix: 'http://172.16.1.83/'
   // apiPrefix: 'http://ear-eye.com/'
   // apiPrefix: 'api/'
   // apiPrefix: '/'
